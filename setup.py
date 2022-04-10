d673c1ce1081f632bffbeed33c96c955bd48be5c
chefsale <sandi.fatic@gmail.com>
2022-04-10 22:26:04 +0100

    bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 META_DATA = dict(name="near-client",
-                 version="0.1.2",
+                 version="0.1.3",
                  license="MIT",
                  author="NEAR Inc",
                  url="https://github.com/calimero-is-near/near-py-client",
