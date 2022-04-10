98518461e3f7fcba0ec86974cef278241fa14363
chefsale <sandi.fatic@gmail.com>
2022-04-10 22:21:12 +0100

    version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 META_DATA = dict(name="near-client",
-                 version="0.1.0",
+                 version="0.1.1",
                  license="MIT",
                  author="NEAR Inc",
                  url="https://github.com/near/near-api-py",
